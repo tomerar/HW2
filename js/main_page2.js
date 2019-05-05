3d5a6351fc090a128c324f73475790c6a02c3f63
tomerar <45894448+tomerar@users.noreply.github.com>
2019-05-05 10:51:51 +0300

    Add files via upload
    fix

--- js/main_page2.js
+++ js/main_page2.js
@@ -25,16 +25,17 @@ function base_conver() {
     }
     clearinput(0);
 
-    var e = document.getElementById("selet_base");
-    
+    var e = document.getElementById("selet_base_from");
+    var e2 = document.getElementById("selet_base_to");
     var strUser = e.options[e.selectedIndex].value;
+    var strUser2 = e.options[e2.selectedIndex].value;
     var initial_base =10;
     
     result =parseInt(value,strUser );
     console.log(parseInt(value, strUser));
 
-    document.getElementById('result_base').innerHTML = "result: " +parseInt(value + '', strUser)
-    .toString(initial_base);
+    document.getElementById('result_base').innerHTML = "result: "+ ConvertBase(value, strUser, strUser2);
+   
 }
 /*check the input*/ 
 function clearinput(res) {
@@ -47,4 +48,11 @@ function clearinput(res) {
         document.getElementById('wrong_input').innerHTML = "";
     }
 
+}
+function ConvertBase(val, base1, base2) {
+    if (typeof (val) == "number") {
+        return parseInt(String(val)).toString(base2);
+    } else {
+        return parseInt(val.toString(), base1).toString(base2)
+    };
 }
